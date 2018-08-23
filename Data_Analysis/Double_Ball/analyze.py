d6e226bbd9657817139e70a2935616d26cc1b805
Hello-Linux <hello_linux@aliyun.com>
2018-08-23 19:54:48 +0800

    修改双色球soup函数

--- Data_Analysis/Double_Ball/analyze.py
+++ Data_Analysis/Double_Ball/analyze.py
@@ -16,7 +16,7 @@ import time
 import csv
 
 URL = 'http://kaijiang.zhcw.com/zhcw/html/ssq/list_%s.html'
-URLS = (URL % i for i in range(1, 10))
+URLS = (URL % i for i in range(1, 5))
 ALLURLS = list(URLS)
 
 HEADERS = {'Connection': 'keep-alive',
@@ -32,28 +32,26 @@ COOKIES = {'Cookie': 'Hm_lvt_692bd5f9c07d3ebd0063062fb0d7622f=1521165246; \
            _ga=GA1.2.1791969145.1521165246;\
            _gid=GA1.2.1551924898.1521165246'}
 
-for i in ALLURLS:  # 遍历网址并执行以下操作
-    time.sleep(3)  # 不能捣乱哦，必须加一个延时
+for i in ALLURLS:
+    time.sleep(3)
     r = requests.get(str(i), headers=HEADERS, cookies=COOKIES)
-    r.encoding == 'utf-8'  # 解决乱码的问题
+    r.encoding == 'utf-8'
+    soup = BeautifulSoup(r.text, "lxml")
+    data = soup.select("tr td")
+    Lottery_dates = data[::7]
+    issue = data[1::7]
+    number = soup.select("tr td em")
+    red1 = number[0::7]
+    red2 = number[1::7]
+    red3 = number[2::7]
+    red4 = number[3::7]
+    red5 = number[4::7]
+    red6 = number[5::7]
+    blue1 = number[6::7]
 
-    soup = BeautifulSoup(r.text, "lxml")  # 转化成LXML格式
-    ceshi = soup.select("tr td")
-    ceshi1 = ceshi[::7]  # 因为标签的问题，采用切片的方式获得想要的数据，也可以用find__all加正则采集
-    ceshi2 = ceshi[1::7]
-    haoma = soup.select("tr td em")
-    hong1 = haoma[0::7]
-    hong2 = haoma[1::7]
-    hong3 = haoma[2::7]
-    hong4 = haoma[3::7]
-    hong5 = haoma[4::7]
-    hong6 = haoma[5::7]
-    lan1 = haoma[6::7]
-
-    print(i)  # 用控制台监视已经爬过了多少网址
-
-    for i, j, h1, h2, h3, h4, h5, h6, l1 in zip(ceshi1, ceshi2, hong1, hong2, hong3, hong4, hong5, hong6, lan1):
-        with open('egg.csv', 'a', newline="") as csvfile:
+    print("地址:%s 已经分析完毕"% (i))
+    for i, j, r1, r2, r3, r4, r5, r6, b1 in zip(Lottery_dates, issue, red1, red2, red3, red4, red5, red6, blue1):
+        with open('data.csv', 'a', newline="") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(
-                [i.string, j.string, h1.string, h2.string, h3.string, h4.string, h5.string, h6.string, l1.string])
+                [i.string, j.string, r1.string, r2.string, r3.string, r4.string, r5.string, r6.string, b1.string])
