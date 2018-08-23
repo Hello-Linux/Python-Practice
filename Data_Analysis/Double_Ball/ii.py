6f4b4ccd4c339b3c80e7c8551de6fd441c04b856
Sun~shell <hello_linux@aliyun.com>
2018-08-23 19:57:49 +0800

    删除抓取数据
    删除抓取数据

--- Data_Analysis/Double_Ball/ii.py
+++
@@ -1,8 +0,0 @@
-#!/bin/env python3.7
-#   Version: v1.0.1
-#   Filename: ii.py
-#   Author: {{ author }}
-#   Description: ---
-#   Create: 2018-08-23 15:00:13
-a = [x for x in range(1,100)]
-print(a[::3])
